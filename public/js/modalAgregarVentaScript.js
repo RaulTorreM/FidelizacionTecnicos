990c881f5361d4076716e9d0e80cb72e5c528a94
Josué García <77043114@continental.edu.pe>
2024-08-09 13:40:09 -0500

    Agregando los inputs al form del modal agregarNuevaVenta

--- public/js/modalAgregarVentaScript.js
+++ public/js/modalAgregarVentaScript.js
@@ -1,4 +1,25 @@
-let idVentaIntermediadaXML = [];
+let ventaIntermediadaObject = {};
+let idVentaIntermediadaInput = document.getElementById('ventaIntermediadaId');
+let idTecnicoInput = document.getElementById('tecnicoId');
+let nombreTecnicoInput = document.getElementById('tecnicoNombre');
+let tipoCodigoClienteInput = document.getElementById('tipoCodigoCliente');
+let codigoClienteInput = document.getElementById('clienteId');
+let nombreClienteInput = document.getElementById('clienteNombre');
+let fechaHoraEmisionInput = document.getElementById('fechaHoraEmisionVentaIntermediada');
+let montoTotalInput = document.getElementById('montoTotal');
+let puntosGanadosInput = document.getElementById('puntosGanadosInput');
+
+let formInputsArray = [
+    idVentaIntermediadaInput, 
+    idTecnicoInput, 
+    nombreTecnicoInput, 
+    tipoCodigoClienteInput,
+    codigoClienteInput,
+    nombreClienteInput,
+    fechaHoraEmisionInput,
+    montoTotalInput,
+    puntosGanadosInput,
+];
 
 function selectOptionAgregarVenta(value, idInput, idOptions) {
     //Colocar en el input la opción seleccionada 
@@ -15,6 +36,9 @@ function selectOptionAgregarVenta(value, idInput, idOptions) {
         document.getElementById('tecnicoId').value = "";
         document.getElementById('tecnicoNombre').value = "";
     }
+
+    var nuevaVentaMessageError = document.getElementById('nuevaVentaMessageError');
+    nuevaVentaMessageError.classList.remove('shown'); 
 }
 
 function validateValueOnRealTime(input) {
@@ -32,6 +56,7 @@ function validateValueOnRealTime(input) {
 
     if (value === "") {
         console.log("El campo Técnico está vacío");
+        nuevaVentaMessageError.classList.remove('shown'); 
     } else {
         if (!tecnicoEncontrado) {
             console.log("No se encontró el técnico buscado");
@@ -69,6 +94,10 @@ function getAllIdNombreTecnicos() {
 }
 
 function analizarXML(file) {
+    /*
+    HACER PRUEBA UNITARIA PARA ESTA FUNCIÓN 
+    */
+
     const reader = new FileReader();
 
     reader.onload = function(event) {
@@ -79,9 +108,7 @@ function analizarXML(file) {
         const xmlDoc = parser.parseFromString(xmlText, "application/xml");
 
         // Obtener valores
-        const idVentaIntermediada = {
-            id: getElementText(xmlDoc, 'cbc', 'ID')
-        };
+        const idVentaIntermediada = getElementText(xmlDoc, 'cbc', 'ID');
 
         const cliente = {
 			codigoCliente: getElementText(xmlDoc, 'cbc', 'ID', 'cac:AccountingCustomerParty', 'cac:Party', 'cac:PartyIdentification'),
@@ -99,18 +126,29 @@ function analizarXML(file) {
         const tipoCodigoCliente = detectarTipoCodigoCliente(cliente.codigoCliente);
 
 		// Crear un array con los valores
-        idVentaIntermediadaXML = [
-            idVentaIntermediada,
-			tipoCodigoCliente,
-            cliente.codigoCliente,
-            cliente.nombreCliente,
-            fechaHoraEmision.fecha,
-			fechaHoraEmision.hora,
-            montoTotal
-        ];
+        ventaIntermediadaObject = {
+            idVentaIntermediada: idVentaIntermediada,
+            tipoCodigoCliente: tipoCodigoCliente,
+            clienteCodigo: cliente.codigoCliente,
+            clienteNombre: cliente.nombreCliente,
+            fechaEmision: fechaHoraEmision.fecha,
+            horaEmision: fechaHoraEmision.hora,
+            montoTotal: montoTotal,
+            puntosGanados: Math.round(parseFloat(montoTotal)),
+        }
 
-		// Imprimir en consola
-		console.log('idVentaIntermediadaXML:', idVentaIntermediadaXML);
+		// Imprimir en consola usando Object.entries()
+        Object.entries(ventaIntermediadaObject).forEach(([key, value]) => {
+            console.log(`${key}: ${value}`);
+        });
+
+        if (!idTecnicoInput.value.trim() || !nombreTecnicoInput.value.trim()) {
+            console.log("Tiene que rellenar los campos del técnico");
+            clearSomeHiddenInputs();
+        } else {
+            console.log("Sí se rellenó los campos del técnico");
+            fillSomeHiddenInputs(ventaIntermediadaObject);
+        }
     };
 
     reader.onerror = function(event) {
@@ -118,6 +156,24 @@ function analizarXML(file) {
     };
 
     reader.readAsText(file);
+}
+
+function clearSomeHiddenInputs() {
+    formInputsArray.forEach(input => {
+        if (input) {
+            input.value = "";
+        }
+    });
+}
+
+function fillSomeHiddenInputs(ventaIntermediadaObject) {
+    idVentaIntermediadaInput.value = ventaIntermediadaObject.idVentaIntermediada || '';
+    tipoCodigoClienteInput.value = ventaIntermediadaObject.tipoCodigoCliente || '';
+    codigoClienteInput.value = ventaIntermediadaObject.clienteCodigo || '';
+    nombreClienteInput.value = ventaIntermediadaObject.clienteNombre || '';
+    fechaHoraEmisionInput.value = ventaIntermediadaObject.fechaEmision + " " + ventaIntermediadaObject.horaEmision || '';
+    montoTotalInput.value = ventaIntermediadaObject.montoTotal || '';
+    puntosGanadosInput.value = ventaIntermediadaObject.puntosGanados || ''; 
 }
 
 function getElementText(xmlDoc, prefix, tagName, ...path) {
@@ -153,8 +209,26 @@ function detectarTipoCodigoCliente(codigoCliente) {
     return codigoCliente.length === 8 ? 'DNI' : codigoCliente.length === 11 ? 'RUC' : 'Desconocido';
 }
 
+function validarCamposFormulario() {
+    let allFilled = true;
+    formInputsArray.forEach(input => {
+        if (!input.value.trim()) {
+            allFilled = false;
+            input.classList.add('error'); // Añadir una clase de error para mostrar un mensaje
+        } else {
+            input.classList.remove('error');
+        }
+    });
+    return allFilled;
+}
+
 function guardarModalAgregarVenta(idModal, idForm) {
-	
-	//Enviar el formulario
-	//guardarModal(idModal, idForm);
+     // Validar campos antes de enviar
+    if (validarCamposFormulario()) {
+        // Si la validación es correcta, enviar el formulario
+        //guardarModal(idModal, idForm);
+        console.log("Enviando formulario correctamente.")
+    } else {
+        console.log("Todos los campos del formulario deben estar rellenados correctamente.")
+    }
 }
--- resources/views/modals/modalAgregarVenta.blade.php
+++ resources/views/modals/modalAgregarVenta.blade.php
@@ -1,6 +1,6 @@
 <div id="modalAgregarVenta" class="modal">
-    <div class="modal-dialog">
-        <div class="modal-content">
+    <div class="modal-dialog" id="modalAgregarVenta-dialog">
+        <div class="modal-content" id="modalAgregarVenta-content">
             <div class="modal-header">
                 <h5 class="modal-title">Nueva Venta Intermediada</h5>
                 <button class="close" onclick="closeModal('modalAgregarVenta')">&times;</button>
@@ -9,41 +9,78 @@
                 <!-- Formulario para agregar nueva venta -->
                 <form id="formAgregarVenta" action="{{ route('ventasIntermediadas.store') }}" method="POST">
                     @csrf
-                    <!-- Campos ocultos para id y nombre del técnico -->
-                    <input id="tecnicoId" name="tecnicoId">
-                    <input id="tecnicoNombre" name="tecnicoNombre">
+
+                    <!-- Campos ocultos para el formulario -->
+                   
+                    <input type="hidden" id="tecnicoId" name="idTecnico">
+                    <input type="hidden" id="tecnicoNombre" name="nombreTecnico">
+                   
                     <div class="form-group">
                         <label class="primary-label" id="idLabelTecnico">
                             Técnico 
                             <a onclick="openModal('modalAgregarNuevoTecnico')">[+ Nuevo]</a>
                         </label>
                     </div>
-                    <div class="input-select" id="tecnicoSelect">
-                        @php
-                            $idInput = 'tecnicoInput';
-                            $idOptions = 'tecnicoOptions';
-                        @endphp
-                        <input class="input-select-item" type="text" id="tecnicoInput" placeholder="DNI - Nombre"
-                               oninput="filterOptions('{{ $idInput }}', '{{ $idOptions }}'), validateRealTimeInputLength(this, 60),
-                                        validateValueOnRealTime(this)" 
-                               onclick="toggleOptions('{{ $idInput }}', '{{ $idOptions }}')">
-                        <ul class="select-items" id="tecnicoOptions">
-                            @foreach ($tecnicos as $tecnico)
-                                @php
-                                    $value = $tecnico->idTecnico . " - " . $tecnico->nombreTecnico;
-                                @endphp
-                                <li onclick="selectOptionAgregarVenta('{{ $value }}', '{{ $idInput }}', '{{ $idOptions }}')">
-                                    {{ $value }}
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-
                     <div class="form-group start">
+                        <div class="input-select" id="tecnicoSelect">
+                            @php
+                                $idInput = 'tecnicoInput';
+                                $idOptions = 'tecnicoOptions';
+                            @endphp
+                            <input class="input-select-item" type="text" id="tecnicoInput" placeholder="DNI - Nombre"
+                                oninput="filterOptions('{{ $idInput }}', '{{ $idOptions }}'), validateRealTimeInputLength(this, 60),
+                                            validateValueOnRealTime(this)" 
+                                onclick="toggleOptions('{{ $idInput }}', '{{ $idOptions }}')">
+                            <ul class="select-items" id="tecnicoOptions">
+                                @foreach ($tecnicos as $tecnico)
+                                    @php
+                                        $value = $tecnico->idTecnico . " - " . $tecnico->nombreTecnico;
+                                    @endphp
+                                    <li onclick="selectOptionAgregarVenta('{{ $value }}', '{{ $idInput }}', '{{ $idOptions }}')">
+                                        {{ $value }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
                         <span class="inline-alert-message" id="nuevaVentaMessageError"> No se encontró el técnico buscado </span>      
                     </div>
-                </form>
+                    <div class="form-group start">
+                        <label class="primary-label" id="labelClienteAgregarVenta"> Cliente </label>
+                    </div>
 
+                    <div class="form-group gap">
+                        <label class="secondary-label"> Tipo de documento </label>
+                        <input class="input-item" id="tipoCodigoCliente" name="tipoCodigoCliente_VentaIntermediada"
+                               oninput="validateRealTimeInputLength(this, 3)" readonly>
+                        <label class="secondary-label"> # Documento </label>
+                        <input class="input-item" id="clienteId" name="codigoCliente_VentaIntermediada"
+                               oninput="validateRealTimeInputLength(this, 11)">
+                        <label class="secondary-label"> Nombre </label>
+                        <input class="input-item" id="clienteNombre" name="nombreCliente_VentaIntermediada"
+                               oninput="validateRealTimeInputLength(this, 60)">
+                    </div>  
+
+                    <div class="form-group start">
+                        <label class="primary-label" id="labelVentaAgregarVenta"> Venta </label>
+                    </div>
+
+                    <div class="form-group gap">
+                        <label class="secondary-label"> Número de comprobante </label>
+                        <input class="input-item" id="ventaIntermediadaId" name="idVentaIntermediada">
+                        <label class="secondary-label"> Fecha y hora de emisión </label>
+                        <input class="input-item" id="fechaHoraEmisionVentaIntermediada" name="fechaHoraEmision_VentaIntermediada">
+                        <label class="secondary-label"> Monto total </label>
+                        <input class="input-item" id="montoTotal" name="montoTotal_VentaIntermediada">
+                    </div>
+                    
+                    <div class="form-group start">
+                        <label class="important-label"> Puntos generados </label>
+                    </div>
+                    
+                    <div class="form-group gap">
+                        <input class="input-item" id="puntosGanadosInput" name="puntosGanados_VentaIntermediada">
+                    </div>
+                </form>
                 <!-- Seleccionar archivos -->
                 <div class="select-files-div" id="idSelectFilesContainer">
                     <div class="fileArea" id="fileArea" class="select-files-div" ondragover="allowDrop(event)"
